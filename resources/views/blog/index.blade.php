19f34f1ecdeb29a9b76dd74d756eb04bdabb46c8
ibrahima DIALLO <ibrahimadiallo@groupeisi.com>
2025-06-12 02:03:15 +0000

    Perform CRUD operation with data validation on model Post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,14 +3,22 @@
 
 @section('content')
     <h1>Index</h1>
-{{--    @dump($posts)--}}
+
+   <div class="container">
+       @if(session('success'))
+          <span class="text-success">{{ session('success') }}</span>
+       @elseif(session('error'))
+           <span class="text-success">{{ session('error') }}</span>
+       @endif
+   </div>
+
     <div class="container m-3">
         @foreach($posts as $post)
             <article class="m-1">
                 <h3>Title: {{ $post->title }}</h3>
                 <h4>Slug: {{ $post->slug }}</h4>
                 <p>
-                    <a href="{{ route('articles.show', ['slug' => $post->slug, 'post' => $post->id]) }}" class="btn btn-primary">Voir plus</a>
+                    <a href="{{ route('articles.show', ['post' => $post->slug]) }}" class="btn btn-primary">Voir plus</a>
                 </p>
             </article>
         @endforeach
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,15 +2,27 @@
 @section('title', $post->title . ' - Blade template')
 
 @section('content')
+ @if(session('success'))
+     <div class="alert alert-success">
+         {{ session('success') }}
+     </div>
+ @endif
     <article>
-        <h2>{{ $post->id }}</h2>
-        <h3>{{ $post->slug }}</h3>
-        <h4>{{ $post->title }}</h4>
+        <h2>ID - {{ $post->id }}</h2>
+        <h3>Title - {{ $post->title }}</h3>
+        <h5>Slug - {{ $post->slug }}</h5>
+        <h6>Content</h6>
         <p>
             {{ $post->content }}
         </p>
-        <p>
-            <a href="{{ route('articles.index') }}" class="btn btn-primary">Retour</a>
-        </p>
+        <div class="d-flex justify-content-between m-3">
+            <a href="{{ route('articles.index') }}" class="btn btn-primary m-3">Retour</a>
+            <a href="{{ route('articles.edit', $post) }}" class="btn btn-info m-3">Modifier</a>
+            <form action="{{ route('articles.delete', $post) }}" method="POST" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cet article ?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger m-3">Delete</button>
+            </form>
+        </div>
     </article>
 @endsection
